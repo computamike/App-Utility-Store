a7c817319c6550809b29dad1d26fff20cde89ea5
Mike Hingley <mike@titaniumbunker.com>
2016-10-20 20:19:22 +0100

    trying to improve code coverage

---
+++ TestAPI/App_Start/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using Open.GI.hypermart;
+using System.Web.Optimization;
+namespace TestAPI.App_Start
+{
+    [TestFixture]
+    public class TestBundleConfiguration
+    {
+
+       
+ 
+
+        [Test]
+        public void TestThatBundleConfigurationCreatesTheCorrectBundles()
+        {
+            Mock<BundleCollection> mockBundleCollection = new Mock<BundleCollection>();
+             
+
+            BundleConfig.RegisterBundles(mockBundleCollection.Object);
+        }
+    }
+}
